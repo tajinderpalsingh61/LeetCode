19935cb2433bc317fc436dc66ddd25b932f63ca0
Tajinder <tajinderpalsingh61@gmail.com>
2022-07-24 02:07:04 +0530

    Solution 704

---
+++ 704_Binary Search.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low, high = 0, len(nums)
+        while low<high:
+            mid = low+ ((high-low)//2)
+            if nums[mid] == target:
+                return mid
+            
+            if target > nums[mid]:
+                low = mid+1
+            else:
+                high = mid                
+                
+        return -1
